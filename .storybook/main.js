b29971dc421708d023794ef51060bb3becab6a6f
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-03 16:31:44 +0700

    config storybook for nextjs and sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,26 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     // '../stories/**/*.stories.mdx',
-    // '../views/**/*.stories.@(js|jsx|ts|tsx)',
+    '../views/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-postcss',
     'storybook-addon-sass-postcss',
+    {
+      name: 'storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
   ],
+  staticDirs: ['../public'],
   framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
 };
