25ee1588e065b285079a4eab01ef6490c0d93318
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-05 17:31:52 +0700

    Create slider component

--- views/Home/mains/Slider/index.jsx
+++ views/Home/mains/Slider/index.jsx
@@ -1,10 +1,14 @@
+// libs
+import classnames from 'classnames';
 import React from 'react';
-
+// layout
 import styles from './styles.module.scss';
 
-const Slider = () => (
-  <div className={styles['slider-wrapper']}>
-    <h1>SLIDER</h1>
+const Slider = ({ banners }) => (
+  <div className={classnames(styles['slider-wrapper'])}>
+    <div className={classnames(styles['slider-inner'])}>
+      <img src={banners[0].imageUrl} alt="banner" width={962} height={388} />
+    </div>
   </div>
 );
 export default Slider;
