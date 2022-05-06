18e30cc37f302b33c61c83bba0211af46e8e73c1
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-06 09:02:30 +0700

    Edit image source of slider

--- views/Home/mains/Slider/index.jsx
+++ views/Home/mains/Slider/index.jsx
@@ -1,14 +1,25 @@
 // libs
 import classnames from 'classnames';
+import Image from 'next/image';
 import React from 'react';
-// layout
+import images from '../../../../constants/images';
+
 import styles from './styles.module.scss';
 
-const Slider = ({ banners }) => (
-  <div className={classnames(styles['slider-wrapper'])}>
-    <div className={classnames(styles['slider-inner'])}>
-      <img src={banners[0].imageUrl} alt="banner" width={962} height={388} />
+const Slider = ({ banners }) => {
+  const banner = banners[0];
+  return (
+    <div className={classnames(styles['slider-wrapper'])}>
+      <div className={classnames(styles['slider-inner'])}>
+        <Image
+          src={banner && banner.thumbnail}
+          blurDataURL={images.blurDataURL}
+          alt="banner"
+          width={962}
+          height={388}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Slider;
---
+++ views/Home/mains/Slider/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Slider from './index';
+
+export default {
+  title: 'Components/Slider',
+  component: Slider,
+};
+export const Template = () => <Slider />;
