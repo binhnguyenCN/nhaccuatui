7a782048f947843456173f7d08c1d614e8586ceb
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-09 11:53:04 +0700

    Create footer component

---
+++ components/Layout/Footer/index.jsx
@@ -0,0 +1,18 @@
+// libs
+import classnames from 'classnames';
+import React from 'react';
+// layout
+import FooterData from '../../../data_source/Footer';
+import FooterContact from './components/FooterContact';
+import FooterCopyright from './components/FooterCopyright';
+import styles from './styles.module.scss';
+
+const Footer = () => (
+  <footer className={classnames(styles['footer-wrapper'])}>
+    <div className={classnames(styles['footer-inner'])}>
+      <FooterContact contactInfo={FooterData.contact} />
+      <FooterCopyright copyright={FooterData.copyright} />
+    </div>
+  </footer>
+);
+export default Footer;
