6b04a12ecc5fffa5b9ad2c8b68e4db4ad904016d
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-09 23:00:05 +0700

    Create Footer component

---
+++ components/Layout/Footer/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Footer from './index';
+
+export default {
+  title: 'Components/Layout/Footer',
+  component: Footer,
+};
+
+export const Template = () => <Footer />;
