15673a497bfd9dead57263226b49ff5d1b3a0815
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-09 09:36:21 +0700

    Fix layout of TopSongItem

--- views/Home/mains/TopSong/TopSongItem/index.jsx
+++ views/Home/mains/TopSong/TopSongItem/index.jsx
@@ -2,15 +2,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import images from '../../../../../constants/images';
 // layouts
 import styles from './styles.module.scss';
 
 const TopSongItem = ({ song }) => (
-  <div className={styles['top-song-item']}>
-    <Link href={song.thumbnail}>
-      <Image src={song.thumbnail} width={152} height={152} />
-    </Link>
-  </div>
+  <li className={styles['top-song-item']} title={song.title}>
+    <div className={styles['top-song-thumbnail']}>
+      <Link href={song.thumbnail}>
+        <Image
+          src={song.thumbnail}
+          width={152}
+          height={152}
+          blurDataURL={images.blurDataURL}
+          alt={song.title}
+        />
+      </Link>
+    </div>
+  </li>
 );
 
 export default TopSongItem;
