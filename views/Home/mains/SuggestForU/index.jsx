f2592699f3fb356c365d07c1cf57212d5396d645
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-09 23:14:38 +0700

    Create SuggestForU component

---
+++ views/Home/mains/SuggestForU/index.jsx
@@ -0,0 +1,24 @@
+// libs
+import { CaretRightOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import React from 'react';
+// layouts
+import colors from '../../../../constants/colors';
+import ContentPlaylist from '../../../../data_source/SuggestForU';
+import styles from './styles.module.scss';
+
+const SuggestForYou = () => (
+  <div className={styles['suggest-foru-wrapper']}>
+    <div className={styles['suggest-foru-inner']}>
+      <h3 className={styles['suggest-title']}>{ContentPlaylist.title}</h3>
+      <p className={styles['suggest-description']}>{ContentPlaylist.desc}</p>
+      <Link href={ContentPlaylist.url}>
+        <div className={styles['suggest-btn']}>
+          <CaretRightOutlined color={colors.primary} />
+          {ContentPlaylist.btn}
+        </div>
+      </Link>
+    </div>
+  </div>
+);
+export default SuggestForYou;
