d985cf21915b25f54df2ec75848627ed856aa749
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-09 22:37:44 +0700

    Create datasource for homepage

---
+++ data_source/Header.js
@@ -0,0 +1,145 @@
+import { DashOutlined } from '@ant-design/icons';
+import React from 'react';
+
+const HeaderData = {
+  menuList: [
+    {
+      title: 'Bài hát',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'Playlist',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'Tuyển tập',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'Video',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'BXH',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'Chủ đề',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: 'Top 100',
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+    {
+      title: <DashOutlined />,
+      subMenu: [
+        {
+          title: 'VIỆT NAM',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'ÂU MỸ',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+        {
+          title: 'CHÂU Á',
+          list: ['Nhạc trẻ', 'Trữ tình', 'Nhạc Trịnh', 'Nhạc Trung'],
+        },
+      ],
+    },
+  ],
+};
+
+export default HeaderData;
---
+++ data_source/SuggestForU.js
@@ -0,0 +1,8 @@
+const ContentPlaylist = {
+  title: 'GỢI Ý DÀNH CHO BẠN',
+  desc: 'Thưởng thức những ca khúc phù hợp nhất với bạn',
+  btn: 'Nghe bài hát',
+  url: '/',
+};
+
+export default ContentPlaylist;
