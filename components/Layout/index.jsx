e7e21da5cf54a592774ef040116f9a78d3c08589
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-11 10:48:07 +0700

    Add Company Info component to Layout

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 // layout
 import Footer from './Footer';
 import Header from './Header';
+import CompanyInfo from './CompanyInfo';
 import styles from './styles.module.scss';
 
 const Layout = ({ children }) => (
   <div className={styles.container}>
     <Header />
     {children}
+    <CompanyInfo />
     <Footer />
   </div>
 );
