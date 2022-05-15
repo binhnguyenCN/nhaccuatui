5ef2e36c34d4e78ce7f4280f87806daed78daa38
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-15 21:01:15 +0700

    Config next/image in storybook

--- constants/images.js
+++ constants/images.js
@@ -11,7 +11,7 @@ const rgbDataURL = (r, g, b) =>
   }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
 
 export default {
-  logoIcon: '/images/logo_new.png',
+  logoIcon: 'images/logo_new.png',
   newVersion: 'images/ic_new.png',
   store: 'images/ic_store.png',
   upload: 'images/ic_upload.png',
