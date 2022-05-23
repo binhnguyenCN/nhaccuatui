c78ef5195bbe7a39c73e0ce1f2034c7a50508978
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-23 10:40:59 +0700

    Update testprovider

--- utils/TestProvider.jsx
+++ utils/TestProvider.jsx
@@ -1,6 +1,17 @@
+// libs
+import { useState } from 'react';
+// others
+import locales from '@/locales';
 import { ProvideLocale } from '@/context/LocaleContext';
 
-const TestProvider = ({ children }) => (
-  <ProvideLocale>{children}</ProvideLocale>
-);
+const TestProvider = ({ children }) => {
+  const [locale, setLocale] = useState('vi');
+  return (
+    <ProvideLocale
+      value={{ locale, setLocale, localDataSource: locales[locale] }}
+    >
+      {children}
+    </ProvideLocale>
+  );
+};
 export default TestProvider;
